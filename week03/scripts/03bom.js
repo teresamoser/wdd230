ee7d9486e8e7e099f74e5e81c1d6f0f786f380ab
Teresa Moser <moserte@byui.edu>
2023-01-22 00:13:59 -0700

    Week 03 BOM files

--- week03/scripts/03bom.js
+++ week03/scripts/03bom.js
@@ -1,21 +1,55 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
+const chaptersArray = document.getChapterList() || [];
 
+chaptersArray.forEach(chapter => {displayList(chapter);})
 button.addEventListener('click', () => {
 if (input.value != '') { 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
-deleteButton.addEventListener('click', function () {
-list.removeChild(li);
-input.focus();
-});
-input.focus();
-input.value = '';	
-}
+    displayList (input.value);
+    chaptersArray.push(input.value);
+    setChapterList();
+    input.value = '';
+    input.focus();
+    } 
+function displayList(item){
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    li.textContent = item;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+    list.append(li);
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+        })
+    }
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+    }
+function getChapterList(){
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+    }
+chapter = chapter.slice(0, chapter.length - 1)
+chaptersArray = chaptersArray.filter((item) => item !== chapter);
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+        }
+    // const li = document.createElement('li');
+    // const deleteButton = document.createElement('button');
+    // li.textContent = input.value;
+    // deleteButton.textContent = '❌';
+    // li.append(deleteButton);
+    // list.append(li);
+    //     deleteButton.addEventListener('click', function () {
+    //     list.removeChild(li);
+    //     input.focus();
+        // });
+    input.focus();
+    input.value = '';	
 });
 
