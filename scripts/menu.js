e9219a1fc5fdbc0cf88ff7ff03cc14441b288b45
Teresa Moser <moserte@byui.edu>
2023-01-22 23:47:30 -0700

    Update menu index

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,6 @@
-const mybtn = document.querySelector("header button");
-const mynav = document.querySelectorAll("nav a");
 
+const mybtn = document.querySelector("header button");
+const mynav = document.querySelectorAll("nav li");
 
 mybtn.addEventListener("click", () => {
 	mynav.forEach((item) => item.classList.toggle("open"));
