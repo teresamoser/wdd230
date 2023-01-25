b4668ee351d32caff3abf2fe2ad421441537270c
Teresa Moser <moserte@byui.edu>
2023-01-24 23:27:15 -0700

    chamber updates

--- Chamber/scripts/Chamber-menu.js
+++ Chamber/scripts/Chamber-menu.js
@@ -1,6 +1,6 @@
 
 const mybtn = document.querySelector("header button");
-const mynav = document.querySelectorAll("nav a");
+const mynav = document.querySelectorAll("nav");
 
 mybtn.addEventListener("click", () => {
 	mynav.forEach((item) => item.classList.toggle("open"));
