0136257ac864a3f68bad2cd6ef766ab2623bffc9
Teresa Moser <moserte@byui.edu>
2023-02-01 23:50:20 -0700

    updates to home page and chamber

--- Chamber/scripts/chamber-visits.js
+++ Chamber/scripts/chamber-visits.js
@@ -1,11 +1,10 @@
+
 // initialize display elements
-const todayDisplay = document.querySelector(".today");
-const visitsDisplay = document.querySelector(".visits");
+const visitsDisplay = document.querySelector("#visits");
 
 // get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
-
-// determine if this is the first visit or display the number of visits.
+ 
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
 } else {
@@ -14,5 +13,7 @@ if (numVisits !== 0) {
 
 // increment the number of visits.
 numVisits++;
+
 // store the new number of visits value
 localStorage.setItem("visits-ls", numVisits);
+
