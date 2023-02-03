27c26e6206921db4d254fcd14a6bb0c5f017e480
Teresa Moser <moserte@byui.edu>
2023-02-02 21:24:14 -0700

    chamber updates

--- Chamber/scripts/chamber-visits.js
+++ Chamber/scripts/chamber-visits.js
@@ -1,10 +1,13 @@
-
 // initialize display elements
 const visitsDisplay = document.querySelector("#visits");
 
 // get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
- 
+let numVisits = Number(window.localStorage.getItem("visits-ls")); 
+// Using the Number() function ensures that if the storage item 
+//does not exist, it will be converted into a zero (0) which 
+//helps on the if block condition.
+
+// determine if this is the first visit or display the number of visits.
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
 } else {
@@ -13,7 +16,5 @@ if (numVisits !== 0) {
 
 // increment the number of visits.
 numVisits++;
-
 // store the new number of visits value
 localStorage.setItem("visits-ls", numVisits);
-
