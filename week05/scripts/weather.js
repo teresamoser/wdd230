45a4a23a9f0b69e8e90deb6081978ffaf48299ff
Teresa Moser <moserte@byui.edu>
2023-02-04 23:41:04 -0700

    Weather app done

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,36 +1,24 @@
-// select HTML elements in the document
-// const currentTemp = document.querySelector('#current-temp');
-// const weatherIcon = document.querySelector('#weather-icon');
-// const captionDesc = document.querySelector('figcaption');
-
-// const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier,DE&appid=595ac2cc14ab1d9e32e1f8e790eaf129&units=imperial';
-
-// async function apiFetch() {
-//     try {
-//       const response = await fetch(url);
-//       if (response.ok) {
-//         const data = await response.json();
-//         // console.log(data); 
-//         displayData(data); 
-//       } else {
-//           throw Error(await response.text());
-//       }
-//     } catch (error) {
-//         console.log(error);
-//     }
-//   } 
-//   apiFetch();
-
-//   function displayResults(data) {
-//     currentTemp.innerHTML = `${data.weather}&deg;F`;
-//     const iconsrc = `https://openweathermap.org/img/w/10d.png`;
-//     let desc = data.weather[0].iconsrc;
-//     weatherIcon.setAttribute('#current-temp', temp);
-//     weatherIcon.setAttribute('#weather-icon', icon);
-//     captionDesc.textContent = `${desc}`;
-//   }
 
 //select HTML elements in the document
-const myTown = document.querySelector()
+const myTown = document.querySelector('#town')
+const myGraphic = document.querySelector('#graphic')
+const myDescription = document.querySelector('#description')
+const myTemperature = document.querySelector('#temperature')
 
+const townName = "Trier"
+const myKey = "595ac2cc14ab1d9e32e1f8e790eaf129"
 
+const myURL = `//api.openweathermap.org/data/2.5/weather?q=${townName}&appid=${myKey}&units=imperial`;
+
+fetch(myURL)
+    .then((response) => response.json())
+    .then((data) => displayData(data));
+
+    function displayData(data) {
+        console.log(data)
+        myTown.innerHTML = data.name
+        myGraphic.src=`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        myGraphic.alt = data.weather[0].main
+        myDescription.innerHTML = data.weather[0].description
+        myTemperature.innerHTML = `${data.main.temp}&deg;F`
+    }
