cce2304e6ae1c352e69a23aa8ec9e5c4fe6a074f
Teresa Moser <moserte@byui.edu>
2023-02-05 20:17:10 -0700

    update home page with weather

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -5,7 +5,7 @@ const myGraphic = document.querySelector('#graphic')
 const myDescription = document.querySelector('#description')
 const myTemperature = document.querySelector('#temperature')
 
-const townName = "Trier"
+const townName = "Rigby"
 const myKey = "595ac2cc14ab1d9e32e1f8e790eaf129"
 
 const myURL = `//api.openweathermap.org/data/2.5/weather?q=${townName}&appid=${myKey}&units=imperial`;
