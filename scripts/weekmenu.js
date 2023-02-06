7e676a26970a208e3aa0c25ca7eda9beecb9bdc7
Teresa Moser <moserte@byui.edu>
2023-02-05 20:59:39 -0700

    update

---
+++ scripts/weekmenu.js
@@ -0,0 +1,16 @@
+function change_myselect(sel) {
+    const dbParam = JSON.stringify({table:sel,limit:20});
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function() {
+      myObj = JSON.parse(this.responseText);
+      text = "<table border='1'>"
+      for (x in myObj) {
+        text += "<tr><td>" + myObj[x].name + "</td></tr>";
+      }
+      text += "</table>"    
+      document.getElementById("demo").innerHTML = text;
+    }
+    xmlhttp.open("POST", "week-activity-links.json", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("x=" + dbParam);
+  }
