a27c2f0b2c1ea745d3a614f84aef85fefc0f9cb2
Teresa Moser <moserte@byui.edu>
2023-02-05 17:55:10 -0700

    Prophet fixes

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -1,37 +1,36 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.table(data.prophets);
+  displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+}
+
+getProphetData();
+
 const displayProphets = (prophets) => {
-    prophets.forEach((prophets) => {
-      // Create elements to add to the div.cards element
-      let card = document.createElement('section');
-      let fullName = document.createElement('h2'); 
-      let portrait = document.createElement('img');
-  
-      // Build the h2 content out to show the prophet's full name
-      fullName.textContent = `${prophets.name} lastname`;
+  // card build code goes here
+  prophets.forEach((prophet) => {
+    // Create elements to add to the div.cards element
+    let card = document.createElement('section');
+    let fullName = document.createElement('h2'); // fill in the blank
+    let portrait = document.createElement('img');
 
-      // Build the image portrait by setting all the relevant attributes
-      portrait.setAttribute('src', prophets.imageurl);
-      portrait.setAttribute('alt', `Portait of ${prophets.name} lastname`); 
-      portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '340');
-      portrait.setAttribute('height', '440');
-  
-      // Append the section(card) with the created elements
-      card.appendChild(fullName); 
-      card.appendChild(portrait);  
-      card.appendChild(card);
-    }); 
-    // end of arrow function and forEach loop
-  
+    // Build the h2 content out to show the prophet's full name
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+    // Build the image portrait by setting all the relevant attributes
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
 
-async function getProphetsData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.table(data.prophets);
-    displayProphets(data.prophets); 
-    }
+    // Append the section(card) with the created elements
+    card.appendChild(fullName); //fill in the blank
+    card.appendChild(portrait);
 
-getProphetsData();
-
+    cards.appendChild(card);
+  });
 }
