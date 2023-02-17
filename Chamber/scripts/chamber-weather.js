195a528bd37382d861646f45a1f9463df3e1965a
Teresa Moser <moserte@byui.edu>
2023-02-16 23:46:06 -0700

    chamber fixes

---
+++ Chamber/scripts/chamber-weather.js
@@ -0,0 +1,24 @@
+
+//select HTML elements in the document
+const myTown = document.querySelector('#town')
+const myGraphic = document.querySelector('#graphic')
+const myDescription = document.querySelector('#description')
+const myTemperature = document.querySelector('#temperature')
+
+const townName = "Rigby"
+const myKey = "595ac2cc14ab1d9e32e1f8e790eaf129"
+
+const myURL = `//api.openweathermap.org/data/2.5/weather?q=${townName}&appid=${myKey}&units=imperial`;
+
+fetch(myURL)
+    .then((response) => response.json())
+    .then((data) => displayData(data));
+
+    function displayData(data) {
+        // console.log(data)
+        myTown.innerHTML = data.name
+        myGraphic.src=`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        myGraphic.alt = data.weather[0].main
+        myDescription.innerHTML = data.weather[0].description
+        myTemperature.innerHTML = `${data.main.temp}&deg;F`
+    }
